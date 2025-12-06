d86f516f293f5740dcdcff28de0e2add5c597efb
Hoang Hai Nam <htccnam@gmail.com>
2025-12-06 16:03:28 +0700

    thêm chức năng đăng nhập
    sửa lại menu và thêm chức năng đăng nhập ( chưa có database )

---
+++ views/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+
+<body>
+    <input type="text" name="Nhập tài khoản" id="user">
+    <br>
+    <input type="text" name="Nhập mật khẩu" id="password">
+    <br>
+    <button onclick="window.location.href='http://localhost/quanlysieuthi/views/menu.php'">đăng nhập</button>
+</body>
+
+</html>
