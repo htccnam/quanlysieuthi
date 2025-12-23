51fce5ce8d93805f03969f41e1a8824345806461
Hoang Hai Nam <htccnam@gmail.com>
2025-12-23 18:39:31 +0700

    menu them sua xoa

--- views/login.php
+++ views/login.php
@@ -1,3 +1,21 @@
+<?php
+include_once("./connectdb.php");
+
+if (isset($_POST['btnDangnhap'])) {
+    $textTaiKhoan = $_POST['txtTaiKhoan'];
+    $textMatKhau = $_POST['txtMatKhau'];
+    $selectTaiKhoan = "SELECT * FROM nhanvien WHERE taikhoan = '$textTaiKhoan' and matkhau = '$textMatKhau'";
+    $resultSelectTaiKhoan = mysqli_query($con, $selectTaiKhoan);
+
+    if ((mysqli_num_rows($resultSelectTaiKhoan) > 0)) {
+        header("Location: menu.php");
+        exit;
+    } else {
+        echo "<script> alert ('tài khoản hoặc mật khẩu không chính xác'); </script>";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,14 +23,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
+    <link rel="stylesheet" href="../css/themsuaxoatimkiem.css">
 </head>
 
 <body>
-    <input type="text" name="Nhập tài khoản" id="user">
-    <br>
-    <input type="text" name="Nhập mật khẩu" id="password">
-    <br>
-    <button onclick="window.location.href='http://localhost/quanlysieuthi/views/menu.php'">đăng nhập</button>
+    <form action="" method="POST">
+        <label for="txtTaiKhoan">Tài khoản</label>
+        <input type="text" name="txtTaiKhoan" required>
+        <br>
+
+        <label for="txtMatKhau">Mật khẩu</label>
+        <input type="text" name="txtMatKhau" required>
+        <br>
+
+        <button name="btnDangnhap">đăng nhập</button>
+    </form>
+
 </body>
 
 </html>
