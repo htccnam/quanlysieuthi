7370d03c0354e5a22186723afece9500977429f9
Hoang Hai Nam <htccnam@gmail.com>
2025-12-24 08:33:29 +0700

    CRUD_nhanvien

--- views/nhanvien/suanhanvien.php
+++ views/nhanvien/suanhanvien.php
@@ -1,3 +1,29 @@
+<?php 
+   include_once("../connectdb.php"); 
+
+   $textMaNhanVien = $_GET['manhanvien'];
+   $sqlSeclectNhanVien = "SELECT * FROM nhanvien WHERE manhanvien='$textMaNhanVien'";
+   $resultSelectNhanVien = mysqli_query($con, $sqlSeclectNhanVien);
+   $rowNhanVien = mysqli_fetch_assoc($resultSelectNhanVien);
+
+   if(isset($_POST['btnSua'])){
+    $textMaNhanVien = $_POST['txtMaNhanVien'];
+    $textTenNhanVien = $_POST['txtTenNhanVien'];
+    $textNgaySinh =$_POST['txtNgaySinh'];
+    $textGioiTinh= $_POST['selectGioiTinh'];
+    $textDiaChi = $_POST['txtDiaChi'];
+    $textSoDienThoai = $_POST['txtSoDienThoai'];
+    $textTaiKhoan = $_POST['txtTaiKhoan'];
+    $textMatKhau =$_POST['txtMatKhau'];
+
+    $sqlCapNhatNhanVien = "UPDATE nhanvien SET tennhanvien ='$textTenNhanVien' , ngaysinh= '$textNgaySinh',gioitinh='$textGioiTinh',diachi='$textDiaChi',sodienthoai='$textSoDienThoai',taikhoan='$textTaiKhoan',matkhau='$textMatKhau' WHERE manhanvien='$textMaNhanVien'";
+    mysqli_query($con,$sqlCapNhatNhanVien);
+
+    echo "<script> alert('Sửa thành công'); 
+        window.location='quanlynhanvien.php';
+    </script>";
+   }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,43 +37,43 @@
         <h1>sửa nhân viên</h1>
         <br>
         <label for="txtMaNhanVien">Mã nhân viên</label>
-        <input type="text" name="txtMaNhanvien" placeholder="Nhập mã nhân viên" readonly>
+        <input type="text" name="txtMaNhanVien" placeholder="Nhập mã nhân viên" value = "<?php echo  $rowNhanVien['manhanvien']  ?>" readonly>
 
         <br>
         <label for="txtTenNhanVien">Tên nhân viên</label>
-        <input type="text" name="txtTennhanvien" placeholder="Nhập tên nhân viên" required>
+        <input type="text" name="txtTenNhanVien" placeholder="Nhập tên nhân viên" value="<?php echo $rowNhanVien['tennhanvien'] ?>" required>
 
         <br>
         <label for="txtNgaySinh">Ngày sinh</label>
-        <input type="date" name="txtNgaySinh" required>
+        <input type="date" name="txtNgaySinh" value="<?php echo $rowNhanVien['ngaysinh'] ?>" required>
 
         <br>
-        <label for="slGioiTinh">Ngày sinh</label>
-        <select name="slGioiTinh" id="">
-            <option value="Nam">Nam</option>
-            <option value="Nu">Nữ</option>
-            <option value="Khac">Khác</option>
+        <label for="slGioiTinh">Giới tính</label>
+        <select name="selectGioiTinh" id="">
+            <option value="Nam" <?php if($rowNhanVien['gioitinh'] == 'Nam') echo 'selected';?>>Nam</option>
+            <option value="Nữ" <?php if($rowNhanVien['gioitinh'] == 'Nữ') echo 'selected'; ?>>Nữ</option>
+            <option value="Khác" <?php if($rowNhanVien['gioitinh'] == 'Khác') echo 'selected' ?>>Khác</option>
         </select>
 
         <br>
         <label for="txtDiaChi">Địa chỉ</label>
-        <input type="text" name="txtDiaChi" placeholder="Nhập địa chỉ" required>
+        <input type="text" name="txtDiaChi" placeholder="Nhập địa chỉ" value="<?php echo $rowNhanVien['diachi'] ?>" required>
 
         <br>
         <label for="txtSoDienThoai">Số điện thoại</label>
-        <input type="number" name="txtSoDienThoai" placeholder="Nhập số điện thoại" required>
+        <input type="number" name="txtSoDienThoai" placeholder="Nhập số điện thoại" value="<?php echo $rowNhanVien['sodienthoai'] ?>" required>
         <br>
 
         <label for="txtTaiKhoan">Tài khoản</label>
-        <input type="text" name="txtTaiKhoan" placeholder="Nhập tài khoản" required>
+        <input type="text" name="txtTaiKhoan" placeholder="Nhập tài khoản" value="<?php echo $rowNhanVien['taikhoan'] ?>" required>
         <br>
 
-        <label for="txtMatKhau">Số điện thoại</label>
-        <input type="text" name="txtMatKhau" placeholder="Nhập mật khẩu" required>
+        <label for="txtMatKhau">Mật khẩu</label>
+        <input type="text" name="txtMatKhau" placeholder="Nhập mật khẩu" value="<?php echo $rowNhanVien['matkhau'] ?>" required>
         <br>
 
-        <button name="btnSua">Sửa</button>
-        <button name="btnThoat" onclick="window.location = 'quanlynhanvien.php'" >Thoát</button>
+        <button name="btnSua" onclick="return confirm('bạn có chắc chắn muốn sửa')">Sửa</button>
+        <button type="button" name="btnThoat" onclick="window.location = 'quanlynhanvien.php'" >Thoát</button>
     </form>
     
 </body>
